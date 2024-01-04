119ba5c684dd63f26962b5970f2af0717f197181
kadeland <sarahwaseem21@gmail.com>
2024-01-04 11:04:19 +0500

    freecodecamp work

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,30 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
-// preserves value
+// useRef
+// preserves value in between the renders
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null);
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    // if(refContainer.current.value) console.log(refContainer.current.value)
+  };
+  console.log(refContainer);
+  useEffect(() => {
+    refContainer.current.focus();
+  });
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSumbit}>
+        <input type="text" ref={refContainer} />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 };
 
 export default UseRefBasics;
